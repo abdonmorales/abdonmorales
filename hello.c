cd33e5541d24b85cce753482bc6af891e6bb3337
Abdon Morales <abdon.morales13_2022@outlook.com>
2021-04-04 20:21:10 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
    printf("Hello, my name is Abdon Morales!. This written in C");
+   printf("Here's my GitLab link to my Linux distrobution source code");
+   printf("https://gitlab.com/morales-research-corporation")
    return 0;
 }
