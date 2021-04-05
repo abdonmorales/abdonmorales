f8348088d9c0c80755be2cd37e8947268e96e859
Abdon Morales <abdon.morales13_2022@outlook.com>
2021-04-04 20:22:10 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print("Hello, my name is Abdon Morales Jr, I am CS HS student at IDEA Montopolis, that wrote this in Python!")
+print("Here's my link to my GitLab: https://gitlab.com/morales-research-corporation")
+print("https://gitlab.com/abdonmorales")
